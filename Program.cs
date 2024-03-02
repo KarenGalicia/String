09bc28b37ed39f5c3e4b74ae6402040d453e32cc
Karen Galicia <159975831+KarenGalicia@users.noreply.github.com>
2024-03-01 18:51:10 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿string nombres = "Jimena Duarte";
+Console.WriteLine(nombres);
+
+//CONCATENACION -INTERPOLACIÓN DE CADENAS 
+int edad = 27;
+String mensaje = $"Hola, {nombres},!Tienes {edad} años bienvenida a ingeniería";
+Console.WriteLine(mensaje);
